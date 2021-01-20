364557fb62684e37733a7f009e0dd64866809cbf
juanics2020 <juanics11@gmail.com>
2021-01-20 17:53:54 +0100

    20-01-2021_2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,15 @@ const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", loadChildren: "./home/home.module#HomePageModule" },
   //Variables que pasamos a la segunda página(nombre que le damos id y tipo)
-  { path: "ver-articulo/:id/:tipo", loadChildren: "./ver-articulo/ver-articulo.module#VerArticuloPageModule" }
+  { path: "ver-articulo/:id/:tipo", loadChildren: "./ver-articulo/ver-articulo.module#VerArticuloPageModule" },
+  {
+    path: 'descripcion',
+    loadChildren: () => import('./descripcion/descripcion.module').then( m => m.DescripcionPageModule)
+  },
+  {
+    path: 'mapa',
+    loadChildren: () => import('./mapa/mapa.module').then( m => m.MapaPageModule)
+  }
 ];
 
 @NgModule({
